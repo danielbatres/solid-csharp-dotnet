efd82ea3dc79e195f9732e16e637a13a9c94ed1c
danielbatres <danielbatresuser@gmail.com>
2023-02-11 15:02:54 -0600

    Base code to apply the liskov substitution principle

---
+++ 3.-LiskovSubstitution/3.-LiskovSubstitution/EmployeeContractor.cs
@@ -0,0 +1,7 @@
+namespace Liskov;
+
+public class EmployeeContractor : Employee {
+  public EmployeeContractor(string fullname, int hoursWorked, int extrahours) : base (fullname, hoursWorked, extrahours) {
+
+  }
+}
---
+++ 3.-LiskovSubstitution/3.-LiskovSubstitution/EmployeeFullTime.cs
@@ -0,0 +1,7 @@
+namespace Liskov;
+
+public class EmployeeFullTime : Employee {
+  public EmployeeFullTime(string fullname, int hoursWorked, int extrahours) : base(fullname, hoursWorked, extrahours) {
+  
+  }
+}
