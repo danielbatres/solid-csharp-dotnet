4cdf4d0d73da623e645aa62acd973f596f680ef9
danielbatres <danielbatresuser@gmail.com>
2023-02-11 13:12:33 -0600

    Open to extensions, due to inheritance with the abstract class

---
+++ 2.-OpenClose/2.-OpenClose/EmployeeContractor.cs
@@ -0,0 +1,16 @@
+namespace _2._OpenClose;
+
+public class EmployeeContractor : Employee {
+
+  public EmployeeContractor(string fullname, int hoursWorked) {
+    Fullname = fullname;
+    HoursWorked = hoursWorked;
+  }
+
+  public override decimal CalculateSalaryMonthly() {
+    decimal hourValue = 20000M;
+    decimal salary = hourValue * HoursWorked;
+
+    return salary;
+  }
+}
--- 2.-OpenClose/2.-OpenClose/Program.cs
+++ 2.-OpenClose/2.-OpenClose/Program.cs
@@ -2,7 +2,8 @@
 
 ShowSalaryMonthly(new List<Employee>() {
   new EmployeeFullTime("Pepito Pérez", 160),
-    new EmployeePartTime("Manuel Lopera", 180)
+    new EmployeePartTime("Manuel Lopera", 180),
+    new EmployeeContractor("Manuel Pérez", 200)
 });
 
 void ShowSalaryMonthly(List<Employee> employees) {
